b28417d83e0e623c813bd913b5c33a06445097d7
Abdulaziz <abdusattorov2004@mail.ru>
2023-08-09 01:59:12 +0500

    dinamic items and pagination

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,39 +12,46 @@
                     @if(auth()->user()->role->name == 'manager' )
 
                     {{ __("Manager") }}
-
-                    <!-- This is an example component -->
-                    <div class='flex items-center justify-center p-4 mt-4'>
-                        <div class="rounded-xl border p-5 shadow-md w-9/12 bg-white">
-                            <div class="flex w-full items-center justify-between border-b pb-3">
-                                <div class="flex items-center space-x-3">
-                                    <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]">
+                    @foreach($applications as $application)
+                        <div class='flex items-center justify-center p-4 mt-4'>
+                            <div class="rounded-xl border p-5 shadow-md w-9/12 bg-white">
+                                <div class="flex w-full items-center justify-between border-b pb-3">
+                                    <div class="flex items-center space-x-3">
+                                        <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]">
+                                        </div>
+                                        <div class="text-lg font-bold text-slate-700">{{$application->user->name}}</div>
                                     </div>
-                                    <div class="text-lg font-bold text-slate-700">Joe Smith</div>
+                                    <div class="flex items-center space-x-8">
+                                        <button
+                                            class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">id: {{$application->id}}</button>
+                                        <div class="text-xs text-neutral-500">  </div>
+                                    </div>
                                 </div>
-                                <div class="flex items-center space-x-8">
-                                    <button
-                                        class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">id: 2</button>
-                                    <div class="text-xs text-neutral-500">  </div>
+
+                                <div class="mt-4 mb-6">
+                                    <div class="mb-3 text-xl font-bold">                                       
+                                          {{$application->subject}}
+                                    </div>
+                                    <div class="text-sm text-neutral-600">                                   
+                                         {{$application->message}}
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="mt-4 mb-6">
-                                <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus
-                                    neque?</div>
-                                <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna.
-                                    Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed
-                                    gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum
-                                    consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec.
-                                    Curabitur eget feugiat massa</div>
-                            </div>
+                                <div class="box flex justify-between">
+                                 <p>
+                                    {{$application->user->email}}
+                                </p>
 
-                            <p>
-                               email@email.com
-                            </p>
+                                <p>
+                                   {{$application->created_at}}
+                                </p>
+                                </div>
+                               
+                            </div>
                         </div>
-                    </div>
+                    @endforeach
 
+                    {{$applications->links()}}
                     
 
                     @else
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,25 @@
 <?php
 
 use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return redirect('dashboard');
-})->middleware('auth');
+Route::group(['middleware' => 'auth'], function(){
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/', [MainController::class, 'main'])->middleware('auth');
+
+    Route::get('/dashboard', [MainController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::resource('applications', ApplicationController::class);
+
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::resource('applications', ApplicationController::class);
 
 require __DIR__.'/auth.php';
