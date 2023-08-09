21caa8286710476bb0ca585f959fb4848eb91ea9
Abdulaziz <abdusattorov2004@mail.ru>
2023-08-09 10:25:40 +0500

    faylni chiqarish

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,12 +32,34 @@
                                     </div>
                                 </div>
 
-                                <div class="mt-4 mb-6">
-                                    <div class="mb-3 text-xl font-bold">                                       
-                                          {{$application->subject}}
+                                <div class="mt-4 mb-6 flex justify-between">
+                                    <div>
+                                        <div class="mb-3 text-xl font-bold">                                       
+                                              {{$application->subject}}
+                                        </div>
+                                        <div class="text-sm text-neutral-600">                                   
+                                             {{$application->message}}
+                                        </div>
                                     </div>
-                                    <div class="text-sm text-neutral-600">                                   
-                                         {{$application->message}}
+
+                                    <div class="border p-6 rounded hover:bg-gray-100 transition cursor-pointer">
+                                   @if(is_null($application->file_url) )
+                                        <p target="_blank">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                                              </svg>                                                
+                                          </p>
+    
+                                    @else
+
+                                    <a href="{{ asset('storage/'.$application->file_url) }}" target="_blank">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                                          </svg>
+                                          
+                                      </a>
+                                    @endif
+                                        
                                     </div>
                                 </div>
 
